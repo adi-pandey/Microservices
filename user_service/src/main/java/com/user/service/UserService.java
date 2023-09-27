dd928f36f61b962e7dfa4bf8ec0273f6aa41b5d3
adi-pandey <adi.pandey1996@gmail.com>
2023-09-27 17:57:13 +0530

    new api added for all user

--- user_service/src/main/java/com/user/service/UserService.java
+++ user_service/src/main/java/com/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.user.service;
 
+import java.util.List;
+
 import com.user.entity.User;
 
 public interface UserService {
 
 	public User getUser(Long id);
+	public List<User> getAllUser();
 }
