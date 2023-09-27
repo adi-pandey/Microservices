5aa9019612c2105075da5a05df5e8596afc3e68a
adi-pandey <adi.pandey1996@gmail.com>
2023-09-27 17:58:15 +0530

    new api added for all contacts

--- contact_service/src/main/java/com/contact/controller/ContactController.java
+++ contact_service/src/main/java/com/contact/controller/ContactController.java
@@ -3,6 +3,7 @@ package com.contact.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,12 @@ public class ContactController {
 	@RequestMapping("/user/{userId}")
 	public List<Contact> getContacts(@PathVariable("userId") Long userId){
 		return this.contactService.getContactofUser(userId);
+		
+	}
+	
+	@GetMapping
+	public List<Contact> getAllContacts(){
+		return contactService.getAllContact();
 		
 	}
 }
